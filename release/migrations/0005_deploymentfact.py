dd3aca2323e9f4320be76461369426e9baf2c61c
Vados <dflz666@gmail.com>
2015-09-04 17:30:09 +0300

    deploy fact

---
+++ release/migrations/0005_deploymentfact.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('release', '0004_auto_20150805_2346'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='DeploymentFact',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('status', models.CharField(choices=[('SC', 'Successful'), ('FL', 'Failed')], max_length=2, default='FL')),
+                ('host', models.CharField(max_length=256)),
+                ('datetime', models.DateTimeField(auto_now_add=True)),
+                ('artifact', models.ForeignKey(to='release.Artifact')),
+                ('environment', models.ForeignKey(to='release.Environment')),
+            ],
+        ),
+    ]
