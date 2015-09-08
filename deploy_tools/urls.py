3b3019eefaad2a0d8239a870371520392bac5a85
Vados <dflz666@gmail.com>
2015-09-08 12:57:44 +0300

    url name

--- deploy_tools/urls.py
+++ deploy_tools/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^(?P<status>plan|history)/(?P<period>month|week)/(?P<year>\d\d\d\d)/(?P<month>[1-9]|1[012])/(?P<day>[1-9]|[12][0-9]|3[01])$', period, name='period'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^deploymentfact/list', fact_list, name='deploymentfact'),
-    url(r'^deploymentfact/request', fact_request, name='test'),
-    url(r'^deploymentfact/create', fact_create, name='fact'),
+    url(r'^deploymentfact/list', fact_list, name='deployment_fact'),
+    url(r'^deploymentfact/request', fact_request, name='fact_request'),
+    url(r'^deploymentfact/create', fact_create, name='fact_create'),
 ]
