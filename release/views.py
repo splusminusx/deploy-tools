3e5b6957ea9775ad29b20ecfff9951dd702be955
Vados <dflz666@gmail.com>
2015-11-09 11:18:30 +0300

    fixes

--- release/views.py
+++ release/views.py
@@ -14,13 +14,37 @@ DAYS = { MONTH: 28, WEEK: 7 }
 DEFAULT_MAX_RELEASE_FOR_DAY = 7
 
 def period(request, status, period, year, month, day):
+
+    def month_inc(year, month):
+        if month < 12:
+            return_date = date(year, month + 1, 1)
+        else:
+            return_date = date(year + 1, 1, 1)
+        return return_date
+
+    def month_reduce(year, month):
+        if month == 0:
+            return_date = date(year - 1, 12, 1)
+        else:
+            return_date = date(year, month - 1, 1)
+        return return_date
+
+    iyear = int(year)
+    imonth = int(month)
+    iday = int(day)
+
     if period == MONTH:
-        number_of_days = monthrange(int(year), int(month))[1]
-        start = date(int(year), int(month), 1)
+        number_of_days = monthrange(iyear, imonth)[1]
+        start = date(iyear, imonth, 1)
+        prev_period = month_reduce(iyear, imonth)
+        next_period = month_inc(iyear, imonth)
     else:
         number_of_days = DAYS[WEEK]
-        start = date(int(year), int(month), int(day))
-        start = start - timedelta(start.weekday())
+        day_of_month = date(iyear, imonth, iday)
+        day_of_week = timedelta(day_of_month.weekday())
+        start = day_of_month - day_of_week
+        prev_period = start - timedelta(number_of_days)
+        next_period = start + timedelta(number_of_days)
 
     if status == PLAN:
         statuses = PLAN_STATUSES
@@ -41,9 +65,6 @@ def period(request, status, period, year, month, day):
     if period == MONTH and max_releases_per_day < DEFAULT_MAX_RELEASE_FOR_DAY:
         max_releases_per_day = DEFAULT_MAX_RELEASE_FOR_DAY
 
-    if period == MONTH:
-        start = date(int(year), int(month), 15)
-
     return render(request, period + '.html', context={
         'day': day,
         'month': month,
@@ -51,8 +72,8 @@ def period(request, status, period, year, month, day):
         'releases': days,
         'days': sorted(days.keys()),
         'max_releases_range': range(0, max_releases_per_day),
-        'prev_period': start - timedelta(number_of_days),
-        'next_period': start + timedelta(number_of_days),
+        'prev_period': prev_period,
+        'next_period': next_period,
         'status': status,
         'period': period,
         'statuses': [PLAN, HISTORY],
