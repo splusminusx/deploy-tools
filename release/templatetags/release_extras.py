7143a8a4f828f9362d16c4a6e84c544b1d086a28
Vados <dflz666@gmail.com>
2015-11-11 17:41:43 +0300

    status color function

--- release/templatetags/release_extras.py
+++ release/templatetags/release_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
@@ -6,3 +7,22 @@ register = template.Library()
 @register.filter
 def index(l, i):
     return l[i]
+
+
+@register.filter
+@stringfilter
+def status_color(status):
+    if status == 'Successful':
+        td = 'success'
+    elif status == 'Failed':
+        td = 'danger'
+    elif status == 'Deploy in progress':
+        td = 'active'
+    elif status == 'New':
+        td = 'info'
+    elif status == 'Ready to deploy':
+        td = 'warning '
+    else:
+        td = ''
+
+    return td
